25de55c25ac0296e1bd09fbdb35ad86a9ed068d4
NotGlebO <77108978+NotGlebO@users.noreply.github.com>
2022-02-17 15:25:00 +0200

    Add files via upload

---
+++ 9_4.py
@@ -0,0 +1,9 @@
+print('Tikai veseli skaitļi')
+a = int(input('Ievadīt sakotneju pakāpe '))
+b = int(input('Ievadīt pedēja pakāpe '))
+i = a
+while i <= b:
+    c = pow(2, a)
+    print(c)
+    i += 1
+    a += 1
